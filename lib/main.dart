cb55748da75ee80d44cf46a6e74d95b95969ca4d
triiikusuma <trikusumaa06@gmail.com>
2024-04-03 15:50:00 +0800

    firts commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _iconBool = false;
+
+  IconData _iconLight = Icons.wb_sunny;
+  IconData _iconDark = Icons.nights_stay;
+
+  ThemeData _lightTheme = ThemeData(
+    primarySwatch: Colors.amber,
+    brightness: Brightness.light,
+    buttonTheme: const ButtonThemeData(
+      buttonColor: Colors.amberAccent,
+    ),
+  );
+
+  ThemeData _darkTheme = ThemeData(
+    primarySwatch: Colors.red,
+    brightness: Brightness.dark,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: _iconBool ? _darkTheme : _lightTheme,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Biodata Mahasiswa"),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  _iconBool = !_iconBool;
+                });
+              },
+              icon: Icon(_iconBool ? _iconDark : _iconLight),
+            ),
+          ],
+      )
+      )
+      );
+  }}
+        
